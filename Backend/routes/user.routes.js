c0caea7e18c8d2036185d210093ab0193932db05
Aman Saini <aman950934@gmail.com>
2025-11-10 23:11:03 +0530

    Pushed full FleetX MERN project

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -7,15 +7,14 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
-    body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+    // body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
+    // body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ],
     userController.registerUser
 )
 
 router.post('/login', [
-    body('email').isEmail().withMessage('Invalid Email'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+    body('email').isEmail().withMessage('Invalid Email')
 ],
     userController.loginUser
 )
